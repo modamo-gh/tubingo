727fb6a4d9bfd3cc204cf17e26247882b083733e
modamo-gh <morganthemosaic@gmail.com>
2025-08-11 20:47:33 -0400

    Finish adding search terms for brand new videos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,260 +1,197 @@
 "use client";
 
+import { searchTerms } from "@/lib/searchTerms";
 import { useEffect, useState } from "react";
 
 const App = () => {
-  const [bingoCardText, setBingoCardText] = useState([
-    "Rabbit-hole channel",
-    "Wholesome content",
-    "Foreign language (not English)",
-    "Liminality",
-    "Potential cult leader",
-    "Rambler",
-    'presented with no context and "doesn\'t make sense"',
-    "Underrated video and/or creator",
-    "Recurring / Established character",
-    "Potential serial killer",
-    'presented with no context but still "makes sense"',
-    "Innovative idea / technology",
-    "Distorted / Bass boosted",
-    "I genuinely feel pity for this individual",
-    "Crappy music",
-    "Random animal spotted",
-    "Channel has over 20,000 uploads",
-    "FREE SPACE",
-    "I learned something new from this vid / channel",
-    "New lead discovery",
-    "Randomly nsfw",
-    "Angry human",
-    "Channel has over 5,000 uploads",
-    "Happy human",
-    "Conspiracy Theory"
-  ]);
-  const searchTerms = [
-    {
-      keyPhrase: "IMG",
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    },
-    {
-      keyPhrase: "MVI",
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    },
-    {
-      keyPhrase: `WIN ${new Date().getFullYear()}${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }, {
-      keyPhrase: `${new Date().getFullYear()}${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }, {
-      keyPhrase: `VID ${new Date().getFullYear()}${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }, {
-      keyPhrase: `${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}${new Date().getFullYear()}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }, {
-      keyPhrase: `Capture ${new Date().getFullYear()}${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }, {
-      keyPhrase: `${String(new Date().getDate()).padStart(
-        2,
-        "0"
-      )}${String(
-        new Date().getMonth() + 1
-      ).padStart(2, "0")}${new Date().getFullYear()}`,
-      searchParameter: "CAI%253D",
-      toString: function () {
-        return `${this.keyPhrase} ${this.searchParameter}`;
-      }
-    }
-  ];
+	const [bingoCardText, setBingoCardText] = useState([
+		"Rabbit-hole channel",
+		"Wholesome content",
+		"Foreign language (not English)",
+		"Liminality",
+		"Potential cult leader",
+		"Rambler",
+		'presented with no context and "doesn\'t make sense"',
+		"Underrated video and/or creator",
+		"Recurring / Established character",
+		"Potential serial killer",
+		'presented with no context but still "makes sense"',
+		"Innovative idea / technology",
+		"Distorted / Bass boosted",
+		"I genuinely feel pity for this individual",
+		"Crappy music",
+		"Random animal spotted",
+		"Channel has over 20,000 uploads",
+		"FREE SPACE",
+		"I learned something new from this vid / channel",
+		"New lead discovery",
+		"Randomly nsfw",
+		"Angry human",
+		"Channel has over 5,000 uploads",
+		"Happy human",
+		"Conspiracy Theory"
+	]);
 
-  const [searchTerm, setSearchTerm] = useState<{
-    keyPhrase: string;
-    searchParameter: string;
-    toString: () => string;
-  }>(searchTerms[Math.floor(Math.random() * searchTerms.length)]);
+	const [searchTerm, setSearchTerm] = useState<{
+		keyPhrase: string;
+		searchParameter: string;
+		toString: () => string;
+	}>(searchTerms.old[Math.floor(Math.random() * searchTerms.old.length)]);
 
-  const [bingoCard, setBingoCard] = useState<boolean[][]>(
-    Array.from({ length: 5 }, () => Array.from({ length: 5 }))
-  );
+	const [bingoCard, setBingoCard] = useState<boolean[][]>(
+		Array.from({ length: 5 }, () => Array.from({ length: 5 }))
+	);
 
-  const setCard = (bingoCardText: string[]) => {
-    const newBingoCard = [...bingoCard];
+	const setCard = (bingoCardText: string[]) => {
+		const newBingoCard = [...bingoCard];
 
-    for (let row = 0; row < newBingoCard.length; row++) {
-      for (let column = 0; column < newBingoCard[row].length; column++) {
-        newBingoCard[row][column] =
-          bingoCardText[row * 5 + column] === "FREE SPACE";
-      }
-    }
+		for (let row = 0; row < newBingoCard.length; row++) {
+			for (let column = 0; column < newBingoCard[row].length; column++) {
+				newBingoCard[row][column] =
+					bingoCardText[row * 5 + column] === "FREE SPACE";
+			}
+		}
 
-    setBingoCard(newBingoCard);
-  };
+		setBingoCard(newBingoCard);
+	};
 
-  const shuffle = () => {
-    const newBingoCardText = [...bingoCardText];
+	const shuffle = () => {
+		const newBingoCardText = [...bingoCardText];
 
-    for (let i = newBingoCardText.length - 1; i > 0; i--) {
-      const j = Math.floor(Math.random() * (i + 1));
+		for (let i = newBingoCardText.length - 1; i > 0; i--) {
+			const j = Math.floor(Math.random() * (i + 1));
 
-      [newBingoCardText[i], newBingoCardText[j]] = [
-        newBingoCardText[j],
-        newBingoCardText[i]
-      ];
-    }
+			[newBingoCardText[i], newBingoCardText[j]] = [
+				newBingoCardText[j],
+				newBingoCardText[i]
+			];
+		}
 
-    setBingoCardText(newBingoCardText);
+		setBingoCardText(newBingoCardText);
 
-    return newBingoCardText;
-  };
+		return newBingoCardText;
+	};
 
-  useEffect(() => {
-    setCard(shuffle());
-  }, []);
+	useEffect(() => {
+		setCard(shuffle());
+	}, []);
 
-  const checkForBingo = () => {
-    for (const row of bingoCard) {
-      if (row.every((cell) => cell)) {
-        console.log("Bingo!");
-      }
-    }
+	const checkForBingo = () => {
+		for (const row of bingoCard) {
+			if (row.every((cell) => cell)) {
+				console.log("Bingo!");
+			}
+		}
 
-    for (let column = 0; column < bingoCard.length; column++) {
-      if (bingoCard.every((_, index) => bingoCard[index][column])) {
-        console.log("Bingo!");
-      }
-    }
+		for (let column = 0; column < bingoCard.length; column++) {
+			if (bingoCard.every((_, index) => bingoCard[index][column])) {
+				console.log("Bingo!");
+			}
+		}
 
-    if (bingoCard.every((row, index) => row[index])) {
-      console.log("Bingo!");
-    }
+		if (bingoCard.every((row, index) => row[index])) {
+			console.log("Bingo!");
+		}
 
-    if (bingoCard.every((row, index) => row[4 - index])) {
-      console.log("Bingo!");
-    }
-  };
+		if (bingoCard.every((row, index) => row[4 - index])) {
+			console.log("Bingo!");
+		}
+	};
 
-  const toggleCell = (index: number) => {
-    if (bingoCardText[index] !== "FREE SPACE") {
-      const newBingoCard = [...bingoCard];
-      const row = Math.floor(index / 5);
-      const column = index % 5;
+	const toggleCell = (index: number) => {
+		if (bingoCardText[index] !== "FREE SPACE") {
+			const newBingoCard = [...bingoCard];
+			const row = Math.floor(index / 5);
+			const column = index % 5;
 
-      newBingoCard[row][column] = !newBingoCard[row][column];
+			newBingoCard[row][column] = !newBingoCard[row][column];
 
-      setBingoCard(newBingoCard);
-    }
-  };
+			setBingoCard(newBingoCard);
+		}
+	};
 
-  return (
-    <div className="bg-slate-800 flex flex-col md:flex-row h-screen gap-2 items-center justify-center p-2 w-screen">
-      <div className="flex flex-col flex-1 gap-2 h-full items-center justify-center w-full">
-        <div className="aspect-video w-full">
-          <iframe
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowFullScreen
-            className="rounded"
-            height="100%"
-            src="https://www.youtube.com/embed/dQw4w9WgXcQ"
-            width="100%"
-          />
-        </div>
-        <button
-          className="bg-slate-300 hover:bg-slate-400 cursor-pointer font-medium p-4 rounded text-slate-800 transition-colors"
-          onClick={() => {
-            const sT = searchTerms[
-              Math.floor(Math.random() * searchTerms.length)
-            ];
-            setSearchTerm(
-              sT
-            );
-            console.log(sT)
-          }}
-        >
-          New Video
-        </button>
-      </div>
-      <div className="flex flex-col flex-1 gap-2 items-center justify-center">
-        <div className="aspect-square grid grid-cols-5 gap-2 max-h-[90vh]">
-          {bingoCardText.map((text, index) => {
-            return (
-              <div
-                className={`aspect-square ${bingoCard[Math.floor(index / 5)][index % 5]
-                  ? "bg-green-500 hover:bg-green-600"
-                  : "bg-slate-200 hover:bg-slate-300"
-                  } cursor-pointer flex items-center justify-center p-2 rounded`}
-                key={index}
-                onClick={() => {
-                  toggleCell(index);
-                  checkForBingo();
-                }}
-              >
-                <p
-                  className={`leading-tight text-center text-slate-800 text-[6px] md:text-[8px] lg:text-xs xl:text-lg`}
-                >
-                  {text}
-                </p>
-              </div>
-            );
-          })}
-        </div>
-        <button
-          className="bg-slate-300 hover:bg-slate-400 cursor-pointer font-medium p-4 rounded text-slate-800 transition-colors"
-          onClick={() => {
-            setCard(shuffle());
-          }}
-        >
-          Generate New Card
-        </button>
-      </div>
-    </div>
-  );
+	return (
+		<div className="bg-slate-800 flex flex-col md:flex-row h-screen gap-2 items-center justify-center p-2 w-screen">
+			<div className="flex flex-col flex-1 gap-2 h-full items-center justify-center w-full">
+				<div className="aspect-video w-full">
+					<iframe
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+						allowFullScreen
+						className="rounded"
+						height="100%"
+						src="https://www.youtube.com/embed/dQw4w9WgXcQ"
+						width="100%"
+					/>
+				</div>
+				<div className="flex justify-around w-full">
+					<button
+						className="bg-slate-300 hover:bg-slate-400 cursor-pointer font-medium p-4 rounded text-slate-800 transition-colors"
+						onClick={() => {
+							const sT =
+								searchTerms.old[
+									Math.floor(
+										Math.random() * searchTerms.old.length
+									)
+								];
+							setSearchTerm(sT);
+							console.log(sT);
+						}}
+					>
+						Brand New Video
+					</button>
+					<button
+						className="bg-slate-300 hover:bg-slate-400 cursor-pointer font-medium p-4 rounded text-slate-800 transition-colors"
+						onClick={() => {
+							const sT =
+								searchTerms.new[
+									Math.floor(
+										Math.random() * searchTerms.new.length
+									)
+								];
+							setSearchTerm(sT);
+							console.log(sT);
+						}}
+					>
+						Old Forgotten Video
+					</button>
+				</div>
+			</div>
+			<div className="flex flex-col flex-1 gap-2 items-center justify-center">
+				<div className="aspect-square grid grid-cols-5 gap-2 max-h-[90vh]">
+					{bingoCardText.map((text, index) => {
+						return (
+							<div
+								className={`aspect-square ${
+									bingoCard[Math.floor(index / 5)][index % 5]
+										? "bg-green-500 hover:bg-green-600"
+										: "bg-slate-200 hover:bg-slate-300"
+								} cursor-pointer flex items-center justify-center p-2 rounded`}
+								key={index}
+								onClick={() => {
+									toggleCell(index);
+									checkForBingo();
+								}}
+							>
+								<p
+									className={`leading-tight text-center text-slate-800 text-[6px] md:text-[8px] lg:text-xs xl:text-lg`}
+								>
+									{text}
+								</p>
+							</div>
+						);
+					})}
+				</div>
+				<button
+					className="bg-slate-300 hover:bg-slate-400 cursor-pointer font-medium p-4 rounded text-slate-800 transition-colors"
+					onClick={() => {
+						setCard(shuffle());
+					}}
+				>
+					Generate New Card
+				</button>
+			</div>
+		</div>
+	);
 };
 
 export default App;
---
+++ src/lib/searchTerms.ts
@@ -0,0 +1,259 @@
+export const searchTerms = {
+	old: [
+		{
+			keyPhrase: "IMG",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: "MVI",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `WIN ${new Date().getFullYear()}${String(
+				new Date().getMonth() + 1
+			).padStart(2, "0")}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `${new Date().getFullYear()}${String(
+				new Date().getMonth() + 1
+			).padStart(2, "0")}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `VID ${new Date().getFullYear()}${String(
+				new Date().getMonth() + 1
+			).padStart(2, "0")}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `${String(new Date().getMonth() + 1).padStart(
+				2,
+				"0"
+			)}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}${new Date().getFullYear()}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `Capture ${new Date().getFullYear()}${String(
+				new Date().getMonth() + 1
+			).padStart(2, "0")}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}${String(new Date().getMonth() + 1).padStart(
+				2,
+				"0"
+			)}${new Date().getFullYear()}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: "My Movie",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: "My Edited Video",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: "/Storage/Emulated/",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: "PXL",
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `InShot ${new Date().getFullYear()}${String(
+				new Date().getMonth() + 1
+			).padStart(2, "0")}${String(new Date().getDate()).padStart(
+				2,
+				"0"
+			)}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `WhatsApp Video ${new Date().getFullYear()}`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `FullSizeRender`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `RPReplay`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `VTS 01`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `DVR`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `SWF`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `VLC Record`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `Robloxapp`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.MP4`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.3GP`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.MOV`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.AVI`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.WMV`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.MKV`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.MPEG`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.FLV`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.FLAC`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		},
+		{
+			keyPhrase: `.WAV`,
+			searchParameter: "CAISBAgCEAE%253D",
+			toString: function () {
+				return `${this.keyPhrase} ${this.searchParameter}`;
+			}
+		}
+	],
+	new: []
+};
